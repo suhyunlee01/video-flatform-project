1230ff692bb15ef407c36956b4a67e1414766e92
suhyunlee <118110277+suhyunlee01@users.noreply.github.com>
2023-02-08 23:55:20 +0900

    edit upload video / video schema

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,7 +1,7 @@
 import Video from "../models/Video"; //Video라는 모델을 import 해줌으로써 video.find() , video.deletOne() 등등등을 실행시킬 수 있게 되었다.
 
 export const home = async(req, res) => {
-        const videos = await Video.find({});
+        const videos = await Video.find({}); //const postUpload에서 .save()로 db에 저장했던 비디오 데이터들을 -> .find()로 이제 찾아와서 홈 화면에 띄울 수 있게 되었다.
         return res.render("home", { pageTitle: "Home", videos});
 };
 
@@ -26,7 +26,21 @@ export const seeVideo = (req, res) =>{
 export const getUpload = (req, res) =>{
     res.render("upload",{pageTitle: "Upload"})
 };
-export const postUpload = (req, res) =>{
-    const title = req.body.upload;
-    res.redirect("/")
+export const postUpload = async(req, res) =>{
+    const {title, description, hashtags} = req.body;
+    try{
+        await Video.create({
+            title,
+            description,
+            createdAt: Date.now(),
+            hashtags: hashtags.split(",").map(word => `#${word}`), // split을 통해 ,로 문자열을 각각 분리하고 map을 통해 word를 인자로 가진 함수를 만들어 word앞에 #가 붙게 한다.
+            meta: {
+                views:0,
+                rating:0,
+            }
+        })
+        return res.redirect("/")
+    }catch(error){
+        res.render("upload",{pageTitle: "Upload", errorMessage:"all fields are required"}); //try의 코드가 에러가 날 경우에는 다시 upload 페이지로 가도록 구현함.
+    }
 };
--- src/models/Video.js
+++ src/models/Video.js
@@ -1,13 +1,13 @@
 import mongoose from "mongoose";
 
-const videoSchema = new mongoose.Schema({
-    title: String,
-    description: String,
-    createdAt: Date,
-    hashtags: [{type:String}],
+const videoSchema = new mongoose.Schema({ //사용자가 개발자도구로 언제든 수정할 수 있는 html말고 js에서도 required나 maxLength등등을 지정함
+    title: {type: String, required:true, trim:true, minLength:1, maxLength: 40},
+    description: {type: String, required:true, trim:true, maxLength: 200},
+    createdAt: { type: Date, required: true, default: Date.now },
+    hashtags: [{ type: String, trim:true }], //hashtag는 여러개의 string이 각각 나올 예정이라 array로 담아냄
     meta: {
-        views: Number,
-        rating: Number,
+        views: {type: Number, default:0, required:true},
+        rating: {type: Number, default:0, required:true}, //default가 있어서 짜피 required이긴 한데 혹시 모르니까 써봄
     }
 })
 
